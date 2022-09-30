481751e6da827b1055a9724808d769257f903954
fokichevaM <juzzii7@gmail.com>
2022-09-30 18:24:50 +0200

    дз 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import image from './img/ua_flag.jpeg';
+
+function Image() {
+	return (
+		<div>
+			<img src={image} alt='ua_flag' />
+			<p>А вона, а вона сидітиме сумна,</p>
+			<p>Буде пити – не п’яніти від дешевого вина.</p>
+			<p>Я співатиму для неї, аж бринітиме кришталь</p>
+			<p>Та хіба зуміє голос подолати цю печаль.</p>
+		</div>
+	)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Hello word</h1>
+				<h2>I hope you enjoy learning React!</h2>
+				<Image />
+			</header>
+		</div>
+	);
 }
 
 export default App;
