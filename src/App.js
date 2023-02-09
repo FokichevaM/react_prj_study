083943ee628e5d33061e18b161cb688118e87082
fokichevaM <juzzii7@gmail.com>
2023-02-09 22:50:55 +0200

    - huy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-// import logo from './logo.svg';
 import React from 'react';
-import './App.css';
-import image from './img/ua_flag.jpeg';
-
-function Image() {
-	return (
-		<div>
-			<img src={image} alt='ua_flag' />
-			<p>А вона, а вона сидітиме сумна,</p>
-			<p>Буде пити – не п’яніти від дешевого вина.</p>
-			<p>Я співатиму для неї, аж бринітиме кришталь</p>
-			<p>Та хіба зуміє голос подолати цю печаль.</p>
-		</div>
-	)
-}
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Hello word</h1>
-				<h2>I hope you enjoy learning React!</h2>
-				<Image />
-			</header>
-		</div>
-	);
+
+
+  return <div>'Пісюн'</div>;
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<LoginPage />} />
+  //   </Routes>
+  // </Router>
+
 }
 
 export default App;
