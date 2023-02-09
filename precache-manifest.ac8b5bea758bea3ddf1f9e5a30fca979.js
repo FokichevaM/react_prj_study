ae3ef4a940cf627f78d660a47f7baf770e29ea7c
fokichevaM <juzzii7@gmail.com>
2023-02-09 23:52:03 +0200

    Updates

---
+++ precache-manifest.ac8b5bea758bea3ddf1f9e5a30fca979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4174996ea2481c6d7fe8",
+    "url": "/react_prj_study/static/css/main.82db0bda.chunk.css"
+  },
+  {
+    "revision": "4174996ea2481c6d7fe8",
+    "url": "/react_prj_study/static/js/main.1b3ba530.chunk.js"
+  },
+  {
+    "revision": "8acc9acd0ae0dedcf5bb",
+    "url": "/react_prj_study/static/js/runtime~main.aabf338f.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/react_prj_study/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/react_prj_study/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "6f0bc045c74a8d557302b8ebf38747ba",
+    "url": "/react_prj_study/index.html"
+  }
+];
